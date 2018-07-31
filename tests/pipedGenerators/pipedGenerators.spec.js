9e29ad601310ddc301617a12355eb36bb5112861
Vygandas Pliasas <vygandas.pliasas@gmail.com>
2018-07-31 18:41:23 +0300

    added test for factorialSeq sequencer

--- tests/pipedGenerators/pipedGenerators.spec.js
+++ tests/pipedGenerators/pipedGenerators.spec.js
@@ -30,6 +30,18 @@ describe('Generator with pipe', () => {
         assert.equal(seq.next(), 26);
     });
 
+    it('should generate correct accumulated values when using factorialSeq sequence', () => {
+        var pipedSeq = pipeSeq(factorialSeq) // 1, 1, 2, 6, 24
+            .pipeline(sumAccumulator) // 1, 2 (1+1), 4(2+2), 10(4+6), 34(10+24)
+            .invoke();
+        var seq = generator(pipedSeq);
+        assert.equal(seq.next(), 1);
+        assert.equal(seq.next(), 2);
+        assert.equal(seq.next(), 4);
+        assert.equal(seq.next(), 10);
+        assert.equal(seq.next(), 34);
+    });
+
     it('should generate accumulated values and check if they are even or not', () => {
         var pipedSeq = pipeSeq(rangeSeq, 2, 3) // 2, 5, 8, 11
             .pipeline(sumAccumulator) // 2, 7 (5+2), 15(7+8), 26(15+11)
